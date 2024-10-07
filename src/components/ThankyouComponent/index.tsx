8e4ae057cb60f12173b1de2f098ff8e47d8daf2d
Akshay Khot <akshaykhot743@gmail.com>
2024-10-07 15:55:37 +0530

    thankyou comp minor change

--- src/components/ThankyouComponent/index.tsx
+++ src/components/ThankyouComponent/index.tsx
@@ -5,7 +5,7 @@ const ThankyouComponent = () => {
     <div className="flex items-center justify-center h-full w-full pt-[100px]">
       <div className="flex flex-col gap-2 items-center">
         <p className="text-gray-600 font-bold">Thankyou for shopping with us, See you soon!</p>
-        <Link to="/" className="home-link">
+        <Link to="/" className="mt-6 text-blue-600 text-base hover:underline">
           Go back to the homepage
         </Link>
       </div>
